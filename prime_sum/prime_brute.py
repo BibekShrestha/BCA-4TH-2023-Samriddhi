ca0b7cb5cdfbbfb71586c79c9138ce6df5564460
Bibek Shrestha <bsr@logpoint.com>
2023-05-22 21:48:36 +0545

    Adds optimized trial division method for prime sum

--- prime_sum/prime_brute.py
+++ prime_sum/prime_brute.py
@@ -1,3 +1,18 @@
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
 def is_prime_inefficient(n):
     if n <= 1:
         return False
@@ -8,6 +23,18 @@ def is_prime_inefficient(n):
         i += 1
     return True
 
+
+def sum_primes_better(n):
+    count = 0
+    num = 2
+    prime_sum = 0
+    while count < n:
+        if is_prime(num):
+            prime_sum += num
+            count += 1
+            
+        num += 1
+    return prime_sum
 
 def sum_primes_brute(n):
     count = 0
@@ -20,3 +47,8 @@ def sum_primes_brute(n):
         num += 1
     return prime_sum
     
+
+
+if __name__ == '__main__':
+    sum_of_primes = sum_primes_better(1000000)
+    print("Sum of the first million prime numbers:", sum_of_primes)
