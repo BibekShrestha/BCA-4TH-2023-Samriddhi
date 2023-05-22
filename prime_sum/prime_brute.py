e85d9225480b8aa3674585d60b77b2a9b8a54a5a
Bibek Shrestha <bsr@logpoint.com>
2023-05-22 23:51:08 +0545

    Add comment for better algorithm.

--- prime_sum/prime_brute.py
+++ prime_sum/prime_brute.py
@@ -7,6 +7,7 @@ def is_prime(n):
     if n % 2 == 0 or n % 3 == 0:
         return False
     i = 5
+    # for n > 5, every prime can be represented as 6k (+-) 1, where k is a integer number
     while i * i <= n:
         if n % i == 0 or n % (i + 2) == 0:
             return False
